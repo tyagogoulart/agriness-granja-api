a58da3e00572b756d970536f41a7aca7ea38923f
Tyago Goulart <tyagogoulartn@gmail.com>
2020-11-08 19:00:08 -0300

    Estrutura inicial do projeto

---
+++ granja-api/config/urls.py
@@ -0,0 +1,46 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include, re_path
+from django.contrib import admin
+from django.views.generic import RedirectView
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Animais API",
+      default_version='v1',
+      description="API Rest que lista os animais de uma granja",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="tyagogoulartn@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+    re_path('^$', RedirectView.as_view(url='redoc/'), name='schema-swagger-ui'),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    path('api/', include('granja.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
